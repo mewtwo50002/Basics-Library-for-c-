a84b054d710bf9b50821844ee9a3d69a00e7a64c
RogueMew <48804205+mewtwo50002@users.noreply.github.com>
2023-10-25 19:23:56 -0700

    potato

---
+++ basics.cpp
@@ -0,0 +1,110 @@
+string lowercaseFirst(string input)
+{
+    input[0] = tolower(input[0]);
+    return input;
+}
+
+string lowercaseAll(string input)
+{
+    for (int i = 0; i < input.size(); i++)
+    {
+        input[i] = tolower(input[i]);
+    }
+
+    return input;
+}
+
+string uppercaseAll(string input)
+{
+    for (int i = 0; i < input.size(); i++)
+    {
+        input[i] = toupper(input[i]);
+    }
+
+    return input;
+}
+
+string uppercaseFirst(string input)
+{
+    if (!input.empty())
+        input[0] = toupper(input[0]);
+    return input;
+}
+
+void newLine()
+{
+    cout << endl;
+}
+
+void clear()
+{
+    cout << "\x1B[2J\x1B[H";
+}
+
+int randomInt(int max)
+{
+    static bool seeded = false;
+    if (!seeded)
+    {
+        srand(time(NULL));
+        seeded = true;
+    }
+    int random = (rand() % max) + 1;
+    return random;
+}
+
+int maxInput(int input, int max)
+{
+    if (input > max)
+    {
+        return max;
+    }
+    else
+    {
+        return input;
+    }
+}
+
+int minInput(int input, int min)
+{
+    if (input < min)
+    {
+        return min;
+    }
+    else
+    {
+        return min;
+    }
+}
+
+void wait(int timeToWait)
+{
+    sleep_for(seconds(timeToWait));
+}
+
+int menuCreator(vector<string> menuChoices, string prompt)
+{
+    string find;
+    int choice;
+    cout << prompt << endl;
+    for (int i = 0; i < menuChoices.size(); i++)
+    {
+        cout << menuChoices[i] << endl;
+    }
+    cin >> find;
+    for (int i = 0; i < menuChoices.size(); i++)
+    {
+        if (uppercaseFirst(lowercaseAll(find)) == uppercaseFirst(lowercaseAll(menuChoices[i])))
+        {
+            return (i + 1);
+        }
+    }
+    choice = 0;
+    return (choice);
+}
+
+string searchParam(string input)
+{
+    return uppercaseFirst(lowercaseAll(input));
+}
+
--- basics.h
+++ basics.h
@@ -7,118 +7,20 @@
 #include <algorithm>
 #include <cctype>
 #include <locale>
-
+#include "basics.cpp"
 using namespace std;
 using namespace std::chrono;
 using namespace std::this_thread;
 
-string lowercaseFirst(string input)
-{
-    input[0] = tolower(input[0]);
-    return input;
-}
-
-string lowercaseAll(string input)
-{
-    for (int i = 0; i < input.size(); i++)
-    {
-        input[i] = tolower(input[i]);
-    }
-
-    return input;
-}
-
-string uppercaseAll(string input)
-{
-    for (int i = 0; i < input.size(); i++)
-    {
-        input[i] = toupper(input[i]);
-    }
-
-    return input;
-}
-
-string uppercaseFirst(string input)
-{
-    if (!input.empty())
-        input[0] = toupper(input[0]);
-    return input;
-}
-
-void newLine()
-{
-    cout << endl;
-}
-
-void clear()
-{
-    cout << "\x1B[2J\x1B[H";
-}
-
-int randomInt(int max)
-{
-    static bool seeded = false;
-    if (!seeded)
-    {
-        srand(time(NULL));
-        seeded = true;
-    }
-    int random = (rand() % max) + 1;
-    return random;
-}
-
-int maxInput(int input, int max)
-{
-    if (input > max)
-    {
-        return max;
-    }
-    else
-    {
-        return input;
-    }
-}
-
-int minInput(int input, int min)
-{
-    if (input < min)
-    {
-        return min;
-    }
-    else
-    {
-        return min;
-    }
-}
-
-void wait(int timeToWait)
-{
-    sleep_for(seconds(timeToWait));
-}
-
-int menuCreator(vector<string> menuChoices, string prompt)
-{
-    string find;
-    int choice;
-    cout << prompt << endl;
-    for (int i = 0; i < menuChoices.size(); i++)
-    {
-        cout << menuChoices[i] << endl;
-    }
-    cin >> find;
-    for (int i = 0; i < menuChoices.size(); i++)
-    {
-        if (uppercaseFirst(lowercaseAll(find)) == uppercaseFirst(lowercaseAll(menuChoices[i])))
-        {
-            return (i + 1);
-        }
-    }
-    choice = 0;
-    return (choice);
-}
-
-string searchParam(string input)
-{
-    return uppercaseFirst(lowercaseAll(input));
-}
-
+string lowercaseFirst(string input);
+string lowercaseAll(string input);
+string uppercaseAll(string input);
+string uppercaseFirst(string input);
+void newLine();;
+void clear();
+int randomInt(int max);
+int maxInput(int input, int max);
+int minInput(int input, int min);
+void wait(int timeToWait);
+int menuCreator(vector<string> menuChoices, string prompt);
+string searchParam(string input);
