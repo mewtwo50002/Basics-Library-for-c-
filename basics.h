4de2911d38fe79eec15fdf493dd18bc687c59a02
RogueMew <48804205+mewtwo50002@users.noreply.github.com>
2023-10-27 10:29:37 -0700

    Created a Header file

---
+++ basics.h
@@ -0,0 +1,31 @@
+#ifndef BASICS_H
+#define BASICS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <algorithm>
+#include <cctype>
+#include <locale>
+
+using namespace std;
+using namespace std::chrono;
+using namespace std::this_thread;
+
+string lowercaseFirst(string input);
+string lowercaseAll(string input);
+string uppercaseAll(string input);
+string uppercaseFirst(string input);
+void newLine();
+void clear();
+int randomInt(int max);
+int maxInput(int input, int max);
+int minInput(int input, int min);
+void wait(int timeToWait);
+int menuCreator(vector<string> menuChoices, string prompt);
+string searchParam(string input);
+
+#endif
